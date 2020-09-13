c488f53bb588ed198d0da84d1d1aac561e981478
Borhan Uddin Bhuiyan <65293124+BorhanUddinBhuiyan@users.noreply.github.com>
2020-09-13 10:38:31 +0600

    file uploaded

---
+++ js/script.js
@@ -0,0 +1,51 @@
+$(document).ready(function () {
+  // navbar
+  $('.nav-button').click(function() {
+    $('.nav-button').toggleClass('change');
+  });
+
+  $(window).scroll(function() {
+    let position = $(this).scrollTop();
+    if(position >= 250) {
+      $('.nav-menu').addClass('custom-navbar');
+      $('.line').addClass('custom-line');
+    } else {
+      $('.nav-menu').removeClass('custom-navbar');
+      $('.line').removeClass('custom-line');
+    }
+  });
+  // // humburger menu
+
+  // $('.hamburger-menu').click(function() {
+  //   $('.hamburger-menu').toggleClass('change');
+  // });
+
+  // lightbox plugin
+  lightbox.option({
+    'resizeDuration': 200,
+    'wrapAround': true
+  });
+  // gallery
+  $(window).scroll(function () {
+    let position = $(this).scrollTop();
+
+    if (position >= 330) {
+      $('.gallery').addClass('change');
+
+    }
+    else {
+      $('.gallery').removeClass('change');
+    }
+  });
+  // famous writers
+  $('.writers-accordion').click(function (event) {
+    if (event.target.id.split('-')[0] === 'button') {
+      $('#book-1').attr('src', 'img/writers/' + event.target.id.split('-')[1] + '-book1.jpg'
+      );
+      $('#book-2').attr('src', 'img/writers/' + event.target.id.split('-')[1] + '-book2.jpg'
+      );
+    }
+  })
+
+
+});
